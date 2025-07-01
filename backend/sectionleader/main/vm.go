008643e05ab094f2650b008634999cb998fd5cde
Tongsheng Wu <tongshengwu0@gmail.com>
2025-07-01 17:58:58 -0400

    added context and goroutine

--- backend/sectionleader/main/vm.go
+++ backend/sectionleader/main/vm.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	firecracker "github.com/firecracker-microvm/firecracker-go-sdk"
@@ -12,25 +13,27 @@ import (
 type VMState int
 
 const (
-    StateActive VMState = iota
-    StatePaused
-    StateStopped
+	StateActive VMState = iota
+	StatePaused
+	StateStopped
 )
 
 type VM struct {
 	Machine *firecracker.Machine
 	Id      MachineUUID
-	State  VMState
+	State   VMState
 	cancel  context.CancelFunc
 }
 
 type VMManager struct {
-	VMs map[MachineUUID]*VM
+	mutex sync.RWMutex
+	VMs   map[MachineUUID]*VM
 }
 
 func NewVMManager() *VMManager {
 	return &VMManager{
-		VMs: make(map[MachineUUID]*VM),
+		mutex: sync.RWMutex{},
+		VMs:   make(map[MachineUUID]*VM),
 	}
 }
 
@@ -40,27 +43,56 @@ func (manager *VMManager) CreateVM() error {
 		logrus.Errorf("failed to spawn VM: %v", err)
 		return err
 	}
-	if cancelFunc == nil || machine == nil{
+	if cancelFunc == nil || machine == nil {
 		return fmt.Errorf("spawnvm return error")
 	}
 	vmPtr := &VM{
 		Machine: machine,
 		Id:      id,
-		State:  StateActive,
+		State:   StateActive,
 		cancel:  cancelFunc,
 	}
+
+	manager.mutex.Lock()
+	defer manager.mutex.Unlock()
+
 	manager.VMs[id] = vmPtr
 	return nil
 }
 
-func (manager *VMManager) PauseVM(id MachineUUID) error {
+func (manager *VMManager) PauseVM(id MachineUUID) {
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*5)
+
+	go func(ctx context.Context, cancelFunc context.CancelFunc, manager *VMManager, id MachineUUID) {
+		defer cancelFunc()
+
+		manager.mutex.Lock()
+		defer manager.mutex.Unlock()
+
+		vmPtr := manager.VMs[id]
+		if vmPtr.State != StateActive {
+			logrus.Errorf("machine not active, cannot be paused, id: %s", id.String())
+			return
+		}
+
+		err := vmPtr.Machine.PauseVM(ctx)
+		if err != nil {
+			logrus.Errorf("pause vm error")
+			return
+		}
+
+		vmPtr.State = StatePaused
+	}(ctx, cancelFunc, manager, id)
+}
+
+func (manager *VMManager) ResumeVM(id MachineUUID) error {
 	vmPtr := manager.VMs[id]
-	if vmPtr.State != StateActive {
-		return fmt.Errorf("machine not active, cannot be paused")	
+	if vmPtr.State != StatePaused {
+		return fmt.Errorf("machine not paused, cannot be resumed, id %s", id.String())
 	}
-	vmPtr.State = StatePaused
-	
-	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second * 5)
+	vmPtr.State = StateActive
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*5)
 
 	go func(ctx context.Context, cancelFunc context.CancelFunc, vmPtr *VM) {
 		defer cancelFunc()
@@ -68,30 +100,40 @@ func (manager *VMManager) PauseVM(id MachineUUID) error {
 		if err != nil {
 			logrus.Errorf("pause vm error")
 		}
-	}(ctx, cancelFunc,vmPtr)
-	
+	}(ctx, cancelFunc, vmPtr)
+
 	return nil
 }
 
-func (manager *VMManager) GracefulShutdownVM(id MachineUUID) error {
-	vmPtr := manager.VMs[id]
-	err := vmPtr.Machine.Shutdown(context.Background())
-	if err != nil {
-		return err
-	}
+func (manager *VMManager) GracefulShutdownVM(id MachineUUID) {
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*5)
 
-	vmPtr.cancel()
-	vmPtr.State = StateStopped
+	go func(ctx context.Context, cancelFunc context.CancelFunc, manager *VMManager, id MachineUUID) {
+		defer cancelFunc()
 
-	return nil
+		manager.mutex.Lock()
+		defer manager.mutex.Unlock()
+
+		vmPtr := manager.VMs[id]
+		defer vmPtr.cancel()
+
+		if vmPtr.State == StateStopped {
+			logrus.Errorf("attempted to shutdown stopped machine, id: %s", id.String())
+			return
+		}
+
+		vmPtr.State = StateStopped
+		err := vmPtr.Machine.Shutdown(context.Background())
+		if err != nil {
+			logrus.Errorf("machine shutdown err, id: %s, err %v, forcing shutdown", id.String(), err)
+			return
+		}
+	}(ctx, cancelFunc, manager, id)
 }
 
 func (manager *VMManager) GracefulShutdownAll() error {
 	for id := range manager.VMs {
-		err := manager.GracefulShutdownVM(id)
-		if err != nil {
-			return err
-		}
+		manager.GracefulShutdownVM(id)
 	}
 	return nil
 }
